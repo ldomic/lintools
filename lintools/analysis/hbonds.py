f7d7d4ca374cedd0fc35434e5d29fcb883ad983f
ldomic <ldomicevica@gmail.com>
2015-12-14 17:08:10 +0000

    Issue #3

--- lintools/analysis/hbonds.py
+++ lintools/analysis/hbonds.py
@@ -6,7 +6,7 @@ from MDAnalysis.analysis import hbonds
 import numpy as np
 
 class HBonds(object):
-    def __init__(self,topol_object,ligand_name):
+    def __init__(self,topol_object, mol2_file):
         self.HDonorSmarts = Chem.MolFromSmarts('[$([N;!H0;v3]),$([N;!H0;+1;v4]),$([O,S;H1;+0]),$([n;H1;+0])]')
         haccep = "[$([O,S;H1;v2]-[!$(*=[O,N,P,S])]),$([O,S;H0;v2]),$([O,S;-]),$([N;v3;!$(N-*=!@[O,N,P,S])]),$([nH0,o,s;+0])]"
         self.HAcceptorSmarts = Chem.MolFromSmarts(haccep) 
@@ -15,18 +15,18 @@ class HBonds(object):
         self.h_bonds = None
         self.hbonds_for_drawing = []
         self.universe = topol_object
-        self.find_donors_and_acceptors_in_ligand(ligand_name)
-        self.analyse_hbonds(ligand_name)
-    def find_donors_and_acceptors_in_ligand(self,ligand_name):
+        self.mol2_file = mol2_file
+        self.find_donors_and_acceptors_in_ligand()
+        self.analyse_hbonds()
+    def find_donors_and_acceptors_in_ligand(self):
         atom_names=[x.name for x in self.universe.ligand]
-        
-        ligand = Chem.MolFromMol2File(ligand_name+".mol2",removeHs=False)
+        ligand = Chem.MolFromMol2File(self.mol2_file,removeHs=False)
         for atom in ligand.GetSubstructMatches(self.HDonorSmarts, uniquify=1):
             self.donors.append(atom_names[atom[0]])
         for atom in ligand.GetSubstructMatches(self.HAcceptorSmarts, uniquify=1):
              self.acceptors.append(atom_names[atom[0]])
-    def analyse_hbonds(self, ligand_name):
-        h = MDAnalysis.analysis.hbonds.HydrogenBondAnalysis(self.universe.universe,"protein","resname "+ligand_name,acceptors=self.acceptors,donors=self.donors)
+    def analyse_hbonds(self):
+        h = MDAnalysis.analysis.hbonds.HydrogenBondAnalysis(self.universe.universe,"protein",'(segid '+str(self.universe.ligand.segids[0])+' and resid '+str(self.universe.ligand.resids[0])+')',acceptors=self.acceptors,donors=self.donors)
         h.run()
         h.generate_table()
         self.h_bonds = h.table
