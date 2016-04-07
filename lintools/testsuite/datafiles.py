56cfae2804f0dc9abf721dab30c0a2da01d529f9
ldomic <ldomicevica@gmail.com>
2016-04-07 15:25:00 +0100

    Test suite update
    Adding tests for Topol_Data class for testing of unit tests

--- lintools/testsuite/datafiles.py
+++ lintools/testsuite/datafiles.py
@@ -10,14 +10,18 @@ from lintools.testsuite.datafiles import *
 __all__ = [
 	"PDB",
 	"LIG_PDB",
-	"MOL2"
+	"MOL2",
+	"MOL_SVG",
+        "MOL_SVG_INIT"
  
 ]
 
 from pkg_resources import resource_filename
 
 PDB = resource_filename(__name__,'data/4XP1.pdb')
-MOL2 = resource_filename(__name__,'data/LIG.mol2')
+MOL2 = resource_filename(__name__,'data/LDP.mol2')
 LIG_PDB = resource_filename(__name__,'data/LIG.pdb')
+MOL_SVG = resource_filename(__name__,'data/molecule.svg')
+MOL_SVG_INIT = resource_filename(__name__,'data/molecule_init.svg')
 # This should be the last line: clean up namespace
 del resource_filename
