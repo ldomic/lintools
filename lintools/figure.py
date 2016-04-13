cf82ef6a862d53532c8f0e08eb58ee31035b83ec
Laura Domicevica <ldomicevica@gmail.com>
2016-04-13 19:53:42 +0100

    Update figure.py

--- lintools/figure.py
+++ lintools/figure.py
@@ -105,7 +105,7 @@ class Figure(object):
         if diagram_type=="amino":
             self.legend = "<g transform='translate(0,"+str(self.molecule.y_dim+20)+")'>"
             if self.tests== False:
-                coord=sys.argv[0][0:-11]+"legends/amino_legend.svg"
+                coord=sys.argv[0][0:-11]+"lintools/legends/amino_legend.svg"
             else:
                 coord=os.getcwd()+"/legends/amino_legend.svg"
             with open(coord,"r") as f:
