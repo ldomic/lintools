a2eda9a3b09175306471777dccce8d889a8bc0cc
ldomic <ldomicevica@gmail.com>
2016-04-15 12:16:52 +0100

    Test occurrence with single traj

--- lintools/testsuite/analysis/test_occurrence.py
+++ lintools/testsuite/analysis/test_occurrence.py
@@ -39,3 +39,36 @@ class TestOccurrence(TestCase):
  'TYR920': ('920', 7, 18),
  'TYR923': ('923', 15, 42)}
         assert_equal(self.topology.dict_of_plotted_res,dict_of_closest_res)
+
+class TestOccurrence1Traj(TestCase):
+    def setUp(self):
+        self.topology = Topol_Data(TOPOLOGY,[TRAJ_20FR],None,0)
+        self.u = self.topology.universe
+        self.ligand = self.u.select_atoms("resname UNK")
+        self.ligand.resname = "LIG"
+        self.ligand.resnames = "LIG"
+        self.topology.define_ligand(self.ligand)
+        self.occurrence = Occurrence_analysis(TOPOLOGY, [TRAJ_20FR], self.ligand, 3.5, 0, self.topology)
+    def tearDown(self):
+        del self.topology
+        del self.occurrence
+    def test_occurrence_analysis_single_traj(self):
+        self.occurrence.get_closest_residues(30)
+        assert_equal(len(self.occurrence.residue_counts),1)
+        assert_equal(self.occurrence.universe.frame_count,[21])
+        assert_equal(self.occurrence.residue_counts[1]["PHE953"],17)
+        dict_of_closest_res = {'GLN916': ('916', 7),
+ 'ILE310': ('310', 17),
+ 'LEU35': ('35', 21),
+ 'MET38': ('38', 15),
+ 'MET39': ('39', 12),
+ 'MET919': ('919', 17),
+ 'PHE306': ('306', 18),
+ 'PHE698': ('698', 11),
+ 'PHE702': ('702', 8),
+ 'PHE948': ('948', 12),
+ 'PHE953': ('953', 17),
+ 'TYR280': ('280', 13),
+ 'TYR920': ('920', 7),
+ 'TYR923': ('923', 15)}
+        assert_equal(self.topology.dict_of_plotted_res,dict_of_closest_res)
