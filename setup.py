a131b166bab25a7c2b945228dc1038ce363001d7
ldomic <ldomicevica@gmail.com>
2016-10-05 17:19:41 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='lintools',
+      version='09.2016',
+      description='Illustrates protein-ligand interactions',
+      url='http://github.com/ldomic/lintools',
+      author='Laura Domicevica',
+      author_email='ldomicevica@gmail.com',
+      license='GPL',
+      packages=['lintools'],
+      zip_safe=False)
