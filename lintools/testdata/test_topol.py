1539e03b27a61be7d20fe3285968941204ad317e
ldomic <ldomicevica@gmail.com>
2016-10-10 09:11:49 +0100

    Add tests

--- lintools/testdata/test_topol.py
+++ lintools/testdata/test_topol.py
@@ -37,3 +37,62 @@ class TestBasic1(TestCase):
                     assert_equal(testlines[i],lines[i])
                     i+=1
 
+
+class TestBasic2(TestCase):
+    def setUp(self):
+        u =MDAnalysis.Universe(AMI_GRO)
+        lig_name = u.select_atoms("resname UNK")
+        self.test_svg = TEST2
+        self.output_name = "test2_1"
+        self.lintools = Lintools(AMI_GRO,[AMI_XTC],AMI_MOL2,lig_name,30,3.5,[1],[None],[None],0.3,"clocks",self.output_name)
+        self.lintools.save_files()
+        self.lintools.data_input_and_res_time_analysis()
+        self.lintools.analysis_of_prot_lig_interactions()
+        self.lintools.plot_residues()
+        self.lintools.draw_figure()
+    def tearDown(self):
+        self.lintools.remove_files()
+        del self.lintools
+
+    def test_ami_clocks(self):
+        #Is the final svg file produced?
+        assert_equal(os.path.isfile(self.output_name+".svg"),True)
+        if os.path.isfile(self.output_name+".svg") == True:
+            with open(self.test_svg,"r") as test:
+                testlines = test.readlines()
+            with open(self.output_name+".svg","r") as output:
+                lines = output.readlines()
+                i=0
+                for line in lines:
+                    assert_equal(testlines[i],lines[i])
+                    i+=1
+
+class TestBasic3(TestCase):
+    def setUp(self):
+        u =MDAnalysis.Universe(PDB_4XP1)
+        lig_name = u.select_atoms("resname LDP")
+        self.test_svg = TEST3
+        self.output_name = "test3_1"
+        self.lintools = Lintools(PDB_4XP1,[],LDP_MOL2,lig_name,0,3.5,[1],[None],[None],0.3,"domains",self.output_name)
+        self.lintools.save_files()
+        self.lintools.data_input_and_res_time_analysis()
+        self.lintools.analysis_of_prot_lig_interactions()
+        self.lintools.plot_residues()
+        self.lintools.draw_figure()
+    def tearDown(self):
+        self.lintools.remove_files()
+        del self.lintools
+
+    def test_ldp_domains(self):
+        #Is the final svg file produced?
+        assert_equal(os.path.isfile(self.output_name+".svg"),True)
+        if os.path.isfile(self.output_name+".svg") == True:
+            with open(self.test_svg,"r") as test:
+                testlines = test.readlines()
+            with open(self.output_name+".svg","r") as output:
+                lines = output.readlines()
+                i=0
+                for line in lines:
+                    assert_equal(testlines[i],lines[i])
+                    i+=1
+
