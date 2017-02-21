21536470f9287e5c9a908d80400ba03dd915513c
ldomic <ldomicevica@gmail.com>
2017-02-21 18:13:35 +0000

    Some fixes to PiStacking analysis

--- lintools/analysis/pistacking.py
+++ lintools/analysis/pistacking.py
@@ -7,41 +7,41 @@ import os
 from timeit import default_timer as timer
 
 """
-The code in this module has been inspired by PLIP (Protein Ligand Interaction Profiler) from Salentin et al. Nucl. Acids Res. (2015). 
+The code in this module has been inspired by PLIP (Protein Ligand Interaction Profiler) from Salentin et al. Nucl. Acids Res. (2015).
 PLIP is originally published under the Apache License v2 and is available at https://github.com/ssalentin/plip
 """
 class PiStacking(object):
     __version__= "09.2016"
     """This module analyses pi-pi interactions - the stacking of aromatic rings. First, aromatic rings are determined using rdkit for both ligand and protein
-    - residues TRP,HIS,PHE,TYR. Then all possible combinations of pi-pi interactions are analysed through the distance between centers of geometry, angle of 
+    - residues TRP,HIS,PHE,TYR. Then all possible combinations of pi-pi interactions are analysed through the distance between centers of geometry, angle of
     the ring plane and offset distance. If these fullfil the requirements for either 'P' - parallel stacking or 'T' - T-shaped stacking the parameters for these
-    interactions are saved in a named tuple. This tuple can be converted into numpy array (make_table) which is then used to determine the total amount of pi-stacking in a 
-    simulation (count_by_time) or determine how many times a certain interaction occcurs (count_by_type). 
+    interactions are saved in a named tuple. This tuple can be converted into numpy array (make_table) which is then used to determine the total amount of pi-stacking in a
+    simulation (count_by_time) or determine how many times a certain interaction occcurs (count_by_type).
 
     Takes:
         * topology data object * - information about system (lintools.Data object)
         * trajectory * - list of trajectories
         * start_frame_num * - list^^ of frame numbers for start of analysis (Opt)
-        * end_frame_num * - list^^ of frame numbers for end of analysis (Opt) 
+        * end_frame_num * - list^^ of frame numbers for end of analysis (Opt)
         * skip * - list^^ of numbers of how many frames should be skipped for this analysis (Opt)
         * analysis_cutoff * - a fraction of simulation time a feature has to be present for to be
             plotted in the final figure
 
-    ^^ The reason these values are lists is because several trajectories can be submitted for 
-    analysis and different values could be required for each simulation. Therefore, similarly 
-    as with trajectories, start, end and skip variables are submitted as lists with values 
+    ^^ The reason these values are lists is because several trajectories can be submitted for
+    analysis and different values could be required for each simulation. Therefore, similarly
+    as with trajectories, start, end and skip variables are submitted as lists with values
     corresponding for each trajectory.
-    
-    Example: trajectory = ["1.xtc","2.xtc"] #Both are 1000 frames, but the user only wants to 
+
+    Example: trajectory = ["1.xtc","2.xtc"] #Both are 1000 frames, but the user only wants to
              analyse second half the the second trajectory
-             start = [0(for the first traj),500(for the second traj)]   
+             start = [0(for the first traj),500(for the second traj)]
              Other values can be left as default.
 
     Output:
         * self.pistacking * - numpy array of pi-stacking interactions searchable by keywords
         * self.pistacking_by_time * - numpy array of total pi-stacking interactions per frame
         * self.pistacking_by_type * - numpy array of total count of each pi-stacking interaction
-        * self.pi_contacts_for_drawing * - list of the pi-stacking interactions to be included 
+        * self.pi_contacts_for_drawing * - list of the pi-stacking interactions to be included
         in the final figure after calculating frequency of each interaction and comparing it to
         analysis cutoff.
          """
@@ -80,8 +80,8 @@ class PiStacking(object):
                 i+=1
 
     def detect_aromatic_rings_in_protein(self):
-        """Use rdkit to detect aromatic rings in protein. A (relatively) simpler case, since only 4 of 20 aa have rings. 
-        Since different forcefields can have different atom names, each of 4 aromatic residues is extracted as PDB file 
+        """Use rdkit to detect aromatic rings in protein. A (relatively) simpler case, since only 4 of 20 aa have rings.
+        Since different forcefields can have different atom names, each of 4 aromatic residues is extracted as PDB file
         and the aromatic rings are detected for each. The case is trickier with TRP, since this residue has two aromatic rings -
         these are considered separately.
         Data is saved in self.rings dictionary."""
@@ -102,7 +102,7 @@ class PiStacking(object):
                 for atom in ring_info.AtomRings()[ring]:
                     atom_names_in_ring.append(arom_aa_mda.atoms[atom].name)
                 self.rings[(ar_resname,ring)]=atom_names_in_ring
-                
+
     def define_all_protein_rings(self):
         """Make MDAnalysis atom selections for rings in protein residues that will be plotted in the final figure - since they are the only ones that
         should be analysed.
@@ -122,7 +122,7 @@ class PiStacking(object):
 
 
     def find_all_pistacking_pairs(self):
-        """Main analysis function. Analyses each frame in the trajectory in search for pi-pi interactions between previously defined rings on 
+        """Main analysis function. Analyses each frame in the trajectory in search for pi-pi interactions between previously defined rings on
         protein residues and ligand molecule. """
         data = namedtuple("pistacking","frame time proteinring ligandring distance angle offset type resname resid segid")
         i=0
@@ -153,16 +153,16 @@ class PiStacking(object):
                             proj2 = self.projection(protein_norm_vec,p.center_of_geometry(),l.center_of_geometry())
                             offset = min(self.euclidean3d(proj1,l.center_of_geometry()), self.euclidean3d(proj2,p.center_of_geometry()))
 
-                            
+
                             if dist < self.max_distance:
                                 if 0 < angle < self.max_angle_dev and offset < self.max_offset:
                                     contacts = data(frame=frame.frame, time=frame.time, proteinring=tuple([a.id for a in p]), ligandring=tuple([a.id for a in l]), distance=dist, angle=angle, offset=offset,
-                                                    type="P",resname=self.protein_rings[prot_ring].residues.resnames[0], 
+                                                    type="P",resname=self.protein_rings[prot_ring].residues.resnames[0],
                                                     resid=self.protein_rings[prot_ring].residues.resids[0], segid=self.protein_rings[prot_ring].residues.segids[0])
                                     self.timeseries.append(contacts)
                                 if 90 - self.max_angle_dev < angle < 90 + self.max_angle_dev and offset < self.max_offset:
                                     contacts = data(frame=frame.frame, time=frame.time, proteinring=tuple([a.id for a in p]), ligandring=tuple([a.id for a in l]), distance=dist, angle=angle, offset=offset,
-                                                    type="T",resname=self.protein_rings[prot_ring].residues.resnames[0], 
+                                                    type="T",resname=self.protein_rings[prot_ring].residues.resnames[0],
                                                     resid=self.protein_rings[prot_ring].residues.resids[0], segid=self.protein_rings[prot_ring].residues.segids[0])
                                     self.timeseries.append(contacts)
                                 print "Pistack_segid: ",self.protein_rings[prot_ring].residues.segids[0]
@@ -178,26 +178,26 @@ class PiStacking(object):
         """Make numpy array from timeseries data."""
         num_records = np.sum([1 for frame in self.timeseries])
         dtype = [
-                ("frame",float),("time",float),("proteinring",int,(1,6)),
-                ("ligand_ring_ids",int,(1,6)),("distance",float),("angle",float),
+                ("frame",float),("time",float),("proteinring",list),
+                ("ligand_ring_ids",list),("distance",float),("angle",float),
                 ("offset",float),("type","|U4"),("resid",int),("resname","|U4"),("segid","|U8") ]
         out = np.empty((num_records,),dtype=dtype)
         cursor=0
         for contact in self.timeseries:
-            out[cursor] = (contact.frame, contact.time,contact.proteinring,contact.ligandring,contact.distance,contact.angle,contact.offset,
-                            contact.type,contact.resid,contact.resname,contact.segid)
+            print contact.proteinring
+            out[cursor] = (contact.frame, contact.time,contact.proteinring,contact.ligandring,contact.distance,contact.angle,contact.offset,contact.type,contact.resid,contact.resname,contact.segid)
             cursor+=1
         return out.view(np.recarray)
-     
-        
+
+
     def count_by_time(self):
-        """Count how many pi-pi interactions occured in each frame. 
+        """Count how many pi-pi interactions occured in each frame.
         Returns numpy array."""
         out = np.empty((len(self.timesteps),), dtype=[('time', float), ('count', int)])
         for cursor,timestep in enumerate(self.timesteps):
             out[cursor] = (timestep,len([x for x in self.timeseries if x.time==timestep]))
         return out.view(np.recarray)
-        
+
     def count_by_type(self):
         """Count how many times each individual pi-pi interaction occured throughout the simulation.
         Returns numpy array."""
@@ -226,7 +226,7 @@ class PiStacking(object):
         return ring_selection
 
     def get_pistacking_frequency(self,analysis_cutoff):
-        """Calculates the frequency of pi-pi interactions throughout simulations. If the frequency exceeds the 
+        """Calculates the frequency of pi-pi interactions throughout simulations. If the frequency exceeds the
         analysis cutoff, this interaction will be plotted in the final figure.
         Takes:
             * analysis_cutoff * - fraction of simulation time a feature has to be present for to be plotted
@@ -236,7 +236,7 @@ class PiStacking(object):
         for traj in self.pistacking_by_type:
             for contact in self.pistacking_by_type[traj]:
                 self.frequency[tuple(map(tuple,contact["ligand_ring_ids"])),contact["type"],contact["resid"],contact["resname"],contact["segid"]]+=contact["frequency"]
-        draw_frequency = {i:self.frequency[i] for i in self.frequency if self.frequency[i]>(int(len(self.trajectory))*analysis_cutoff)}
+        draw_frequency = {i:self.frequency[i] for i in self.frequency if self.frequency[i]>(int(len(self.trajectory))*float(analysis_cutoff))}
 
         self.pi_contacts_for_drawing = {}
         for contact in draw_frequency:
@@ -304,7 +304,3 @@ class PiStacking(object):
             print("Vectors are not in 3D space. Returning None.")
             return None
         return np.sqrt((v1[0] - v2[0]) ** 2 + (v1[1] - v2[1]) ** 2 + (v1[2] - v2[2]) ** 2)
-
-
-
-
